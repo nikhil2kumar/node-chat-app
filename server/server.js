26829cbfa627618106fe643c4ebece0454baf3e2
nikhl2nik <kumarnikhil083@gmail.com>
2018-04-14 19:42:22 +0530

    Add coon ect and disconnect methods

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require('path');
 const express = require('express');
-
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 let app = express();
-
 const port = process.env.PORT || 3000;
-
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected to client');
+    });
+});
+
+server.listen(port, () => {
     console.log(`App is running at server ${port}`);
 });
