7a184008471a1341e1d0db955cfe31518a278124
nikhl2nik <kumarnikhil083@gmail.com>
2018-04-14 19:15:27 +0530

    Adding intial files

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname, '../public');
+
+let app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`App is running at server ${port}`);
+});
